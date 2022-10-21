f4ad51fe22552d940a3a58133ad703e4288a6c5f
kubanychbek <kushtarbekovkuba@gmail.com>
2022-10-22 03:32:02 +0600

    ..

--- src/main/java/com/application/electronic_book/repository/BookRepository.java
+++ src/main/java/com/application/electronic_book/repository/BookRepository.java
@@ -1,8 +1,6 @@
 package com.application.electronic_book.repository;
 
 import com.application.electronic_book.entity.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;

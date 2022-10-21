faab7cbb683efcc5e975626928b62ed352c54eb1
kubanychbek <kushtarbekovkuba@gmail.com>
2022-10-21 21:05:22 +0600

    added response model

--- src/main/java/com/application/electronic_book/model/response/Response.java
+++ src/main/java/com/application/electronic_book/model/response/Response.java
@@ -1,4 +1,25 @@
 package com.application.electronic_book.model.response;
 
-public class Response {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Response<T> {
+    private T data;
+    private long dataAmount;
+    private String error;
+
+    public Response(T data){
+        this.data = data;
+    }
+
+    public Response(String error) {
+        this.error = error;
+    }
+
+    public Response(T data, long dataAmount){
+        this.data = data;
+        this.dataAmount = dataAmount;
+    }
 }
